81a114129be72d309f9f17733e4d1d8021c8386f
EugeneZaharova <eugenezaharova@mail.ru>
2016-11-08 20:32:36 +0400

    Update TTreeItem.h

--- TTreeItem.h
+++ TTreeItem.h
@@ -9,7 +9,6 @@
 template<class T> class TTreeItem {
 public:
 	TTreeItem(std::shared_ptr<T> &obj);
-//	template <class A> friend std::ostream& operator<<(std::ostream& os, const TTreeItem<A>& obj);
 
 	std::shared_ptr<TTreeItem<T>> GetTreeLeft();
 	std::shared_ptr<TTreeItem<T>> GetTreeRight();
@@ -20,29 +19,7 @@ public:
 	virtual ~TTreeItem();
 private:
 	std::shared_ptr<T> obj;
-/*	std::shared_ptr<T> foursquare;
-	std::shared_ptr<T> triangle;*/
 	std::shared_ptr<TTreeItem<T>> left;
 	std::shared_ptr<TTreeItem<T>> right;
 };
-/*
-class TTreeItem {
-public:
-    TTreeItem(const std::shared_ptr<Triangle> &triangle, const std::shared_ptr<Foursquare> &foursquare, const std::shared_ptr<Octagon> &octagon);
-    TTreeItem(const TTreeItem& orig);
-    friend std::ostream& operator<<(std::ostream& os, const TTreeItem& obj);
-    std::shared_ptr<TTreeItem> Gettreeleft();
-    std::shared_ptr<TTreeItem> Gettreerihgt();
-    void delite();
-    std::shared_ptr<TTreeItem> Setnext(std::shared_ptr<TTreeItem> next);
-    std::shared_ptr<TTreeItem> Getnext();
-    std::shared_ptr<Triangle> GetTriangle() const;
-    virtual ~TTreeItem();
-private:
-    std::shared_ptr<Octagon> octagon;
-    std::shared_ptr<Foursquare> foursquare;
-    std::shared_ptr<Triangle> triangle;
-    std::shared_ptr<TTreeItem>  left;
-    std::shared_ptr<TTreeItem> right;
-};*/
 #endif
