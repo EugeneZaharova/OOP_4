1a7b5395cbc7755acab9d9a9384d3a0de582c57c
EugeneZaharova <eugenezaharova@mail.ru>
2016-11-08 20:31:28 +0400

    Update TTree.h

--- TTree.h
+++ TTree.h
@@ -14,25 +14,7 @@ public:
 	void push(std::shared_ptr<T> && obj);
 	bool empty();
 	void print();
-//	std::shared_ptr<T> pop();
-//	template <class A> friend std::ostream& operator<<(std::ostream& os, const TTree<A>& tree);
-//	virtual ~TTree();
 private:
 	std::shared_ptr<TTreeItem<T>> head;
 };
-
-/*class TTree {
-public:
-    TTree();
-    TTree(const TTree& orig);
-
-    void push(std::shared_ptr<Triangle> &&triangle, std::shared_ptr<Foursquare>&&foursquare, std::shared_ptr<Octagon> &&octagon);
-    bool empty();
-    void pop();
-    friend std::ostream& operator<<(std::ostream& os, const TTree& stack);
-    virtual ~TTree();
-private:
-
-    std::shared_ptr<TTreeItem> head;
-};*/
 #endif /*TTREE_H*/
